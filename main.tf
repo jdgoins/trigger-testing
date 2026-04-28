5b524f2c27050052c26702805b09c366ac2f7241
JD Goins <jd.goins@hashicorp.com>
2026-04-28 10:59:33 -0600

    Add test bucket

--- main.tf
+++ main.tf
@@ -12,3 +12,12 @@ resource "null_resource" "three" {
 resource "null_resource" "four" {
   
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-jd-test-bucket-34vgfeiu2yg3fg"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
