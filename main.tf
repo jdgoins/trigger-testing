823b95d7c3f24a3c8faa167a0bdb2bc8a950d076
JD Goins <jd.goins@hashicorp.com>
2026-04-28 11:03:21 -0600

    use ec2

--- main.tf
+++ main.tf
@@ -13,11 +13,27 @@ resource "null_resource" "four" {
   
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-jd-test-bucket-34vgfeiu2yg3fg"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name = "HelloWorld"
   }
 }
