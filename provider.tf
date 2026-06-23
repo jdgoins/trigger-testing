3e570f0c90158318ebd83bf833f9efd524909eee
JD Goins <jd.goins@hashicorp.com>
2026-06-23 14:33:35 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,16 +1,16 @@
-#terraform {
+# terraform {
 #  required_providers {
 #    aws = {
 #      source  = "hashicorp/aws"
 #      version = "~> 6.0"
 #    }
 #  }
-#}
+# }
 
 # Configure the AWS Provider
-#provider "aws" {
+# provider "aws" {
 #  region = "us-west-2"
-#}
+# }
 
 terraform {
   required_version = "1.14.8"
